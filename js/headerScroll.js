216e4dcf17e1babd7b23e06f242a3dceab87adea
ElenaGordienko1 <e2gord97@gmail.com>
2025-06-11 09:52:03 +1000

    правильный скролл шапки сайта

--- js/headerScroll.js
+++ js/headerScroll.js
@@ -1,19 +1,11 @@
-let lastScrollTop = 0;
 const header = document.getElementById('header');
 
 window.addEventListener('scroll', () => {
   const currentScroll = window.scrollY;
 
   if (currentScroll === 0) {
-    
     header.classList.add('hide');
-  } else if (currentScroll > lastScrollTop) {
-
-    header.classList.add('hide');
-  } else if (currentScroll < lastScrollTop) {
-
+  } else {
     header.classList.remove('hide');
   }
-
-  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
 });
